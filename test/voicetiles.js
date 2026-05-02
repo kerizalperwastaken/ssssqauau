bc00dbbfc5d34b6b6819a345b9d0167812f6d214
kerizalperwastaken <105068896+kerizalperwastaken@users.noreply.github.com>
2026-05-03 01:13:23 +0300

    Add plugin to modify VoiceTile border radius

--- test/voicetiles.js
+++ test/voicetiles.js
@@ -1,1 +1,21 @@
+// squareTiles.js
+import { definePlugin } from "@kettu/plugin";
+import { findByName } from "@kettu/metro";
+import { after } from "@kettu/patcher";
 
+export default definePlugin({
+  onLoad() {
+    const VoiceTile = findByName("VoiceTile", false);
+    if (VoiceTile) {
+      this.unpatch = after("render", VoiceTile, (_, ret) => {
+        if (ret?.props?.style) {
+          ret.props.style.borderRadius = 0;
+        }
+        return ret;
+      });
+    }
+  },
+  onUnload() {
+    this.unpatch?.();
+  }
+});
