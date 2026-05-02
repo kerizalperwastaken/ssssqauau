fb95e743daa015bb5c14ce89a63772bdf243f488
kerizalperwastaken <105068896+kerizalperwastaken@users.noreply.github.com>
2026-05-03 01:22:12 +0300

    Update voicetiles.js

--- test/voicetiles.js
+++ test/voicetiles.js
@@ -1,21 +1,40 @@
-// squareTiles.js
-import { definePlugin } from "@kettu/plugin";
-import { findByName } from "@kettu/metro";
-import { after } from "@kettu/patcher";
+const { metro } = vendetta;
+const { findByProps } = metro;
 
-export default definePlugin({
-  onLoad() {
-    const VoiceTile = findByName("VoiceTile", false);
-    if (VoiceTile) {
-      this.unpatch = after("render", VoiceTile, (_, ret) => {
-        if (ret?.props?.style) {
-          ret.props.style.borderRadius = 0;
-        }
-        return ret;
-      });
+// Gerekli stil ve bileşenleri bulalım
+const VoiceTile = findByProps("VoiceUserTile");
+const Styles = findByProps("tile", "videoTile");
+
+export default {
+    name: "SquareVoiceBigCamera",
+    description: "Ses kutucuklarını kare yapar, kamera açanları büyütür.",
+    authors: [{ name: "Gemini", id: "0" }],
+    version: "1.0.0",
+
+    onStart() {
+        // Stil üzerine yama (patch) atıyoruz
+        this.patch = vendetta.patcher.after("render", VoiceTile, ([props], res) => {
+            const isVideo = props.video || props.stream;
+
+            // Kutucuk stillerine müdahale
+            if (res?.props?.style) {
+                if (isVideo) {
+                    // Kamera açanlar: Büyük ve 16:9 oranında
+                    res.props.style.width = "100%";
+                    res.props.style.aspectRatio = 16 / 9;
+                    res.props.style.borderRadius = 12;
+                } else {
+                    // Normal kullanıcılar: Kare
+                    res.props.style.aspectRatio = 1;
+                    res.props.style.width = "48%"; // Yan yana iki kare için
+                    res.props.style.borderRadius = 8;
+                }
+            }
+            return res;
+        });
+    },
+
+    onStop() {
+        this.patch?.();
     }
-  },
-  onUnload() {
-    this.unpatch?.();
-  }
-});
+};
